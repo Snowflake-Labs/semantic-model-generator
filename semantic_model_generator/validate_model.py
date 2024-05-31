1618c8b1bebd791a9baabf7492f87a8c89057f1f
Renee Huang <renee.huang@snowflake.com>
2024-05-31 12:49:04 -0700

    update validate fn

--- semantic_model_generator/validate_model.py
+++ semantic_model_generator/validate_model.py
@@ -1,7 +1,10 @@
 import jsonargparse
 from loguru import logger
 
-from semantic_model_generator.data_processing.proto_utils import yaml_to_semantic_model
+from semantic_model_generator.data_processing.proto_utils import (
+    yaml_to_semantic_model,
+    proto_to_yaml,
+)
 from semantic_model_generator.protos import semantic_model_pb2
 from semantic_model_generator.snowflake_utils.snowflake_connector import (
     SnowflakeConnector,
@@ -10,24 +13,30 @@ from semantic_model_generator.sqlgen.generate_sql import generate_select_with_al
 from semantic_model_generator.validate.context_length import validate_context_length
 
 
-def load_yaml(yaml_path: str) -> semantic_model_pb2.SemanticModel:
+def load_yaml(yaml_path: str) -> str:
+    """
+    Load local yaml file into str.
+
+    yaml_path: str The absolute path to the location of your yaml file. Something like path/to/your/file.yaml.
+    """
     with open(yaml_path) as f:
         yaml_str = f.read()
-    # Validate the context length doesn't exceed max we can support.
-    validate_context_length(yaml_str)
-    model = yaml_to_semantic_model(yaml_str)
-    return model
+    return yaml_str
 
 
-def validate(model: semantic_model_pb2.SemanticModel, snowflake_account: str) -> None:
+def validate(yaml_str: str, snowflake_account: str) -> None:
     """
     For now, validate just ensures that the yaml is correctly formatted and we can parse into our protos.
 
-    yaml_path: str The absolute path to the location of your yaml file. Something like path/to/your/file.yaml.
+    yaml_str: yaml content in string format.
     snowflake_account: str The name of the snowflake account.
 
     TODO: ensure that all expressions are valid by running a query containing all columns and expressions.
     """
+    # Validate the context length doesn't exceed max we can support.
+    validate_context_length(yaml_str)
+
+    model = yaml_to_semantic_model(yaml_str)
     connector = SnowflakeConnector(
         account_name=snowflake_account,
         max_workers=1,
