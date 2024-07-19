e7c1ebb43eb90865c1e40cdfd887ff65afb18227
Chris Nivera <chris.nivera@snowflake.com>
2024-07-19 16:27:19 -0700

    popup if env vars not populated

--- semantic_model_generator/snowflake_utils/env_vars.py
+++ semantic_model_generator/snowflake_utils/env_vars.py
@@ -5,5 +5,28 @@ SNOWFLAKE_ROLE = os.getenv("SNOWFLAKE_ROLE")
 SNOWFLAKE_WAREHOUSE = os.getenv("SNOWFLAKE_WAREHOUSE")
 SNOWFLAKE_USER = os.getenv("SNOWFLAKE_USER")
 SNOWFLAKE_PASSWORD = os.getenv("SNOWFLAKE_PASSWORD")
-SNOWFLAKE_HOST = os.getenv("SNOWFLAKE_HOST")
+SNOWFLAKE_HOST = os.getenv("SNOWFLAKE_HOST")  # optional per README docs
 SNOWFLAKE_AUTHENTICATOR = os.getenv("SNOWFLAKE_AUTHENTICATOR")
+SNOWFLAKE_ACCOUNT_LOCATOR = os.getenv("SNOWFLAKE_ACCOUNT_LOCATOR")
+
+
+def assert_required_env_vars() -> list[str]:
+    """
+    Ensures that the required environment variables are set before proceeding.
+    Returns: list of missing required environment variables
+
+    """
+
+    missing_env_vars = []
+    if not SNOWFLAKE_ROLE:
+        missing_env_vars.append("SNOWFLAKE_ROLE")
+    if not SNOWFLAKE_WAREHOUSE:
+        missing_env_vars.append("SNOWFLAKE_WAREHOUSE")
+    if not SNOWFLAKE_USER:
+        missing_env_vars.append("SNOWFLAKE_USER")
+    if not SNOWFLAKE_ACCOUNT_LOCATOR:
+        missing_env_vars.append("SNOWFLAKE_ACCOUNT_LOCATOR")
+    if not SNOWFLAKE_PASSWORD and not SNOWFLAKE_AUTHENTICATOR:
+        missing_env_vars.append("SNOWFLAKE_PASSWORD/SNOWFLAKE_AUTHENTICATOR")
+
+    return missing_env_vars
