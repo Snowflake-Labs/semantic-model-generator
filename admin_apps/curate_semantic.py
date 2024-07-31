87612e96af6be5b6c1865e4d992b5b0cc901528a
sfc-gh-jsummer <jason.summer@snowflake.com>
2024-07-31 15:34:36 -0500

    Initial commit

---
+++ admin_apps/curate_semantic.py
@@ -0,0 +1,77 @@
+from typing import Optional
+from snowflake.connector import SnowflakeConnection
+
+
+revise_semantic_prompt = """You are a data analyst tasked with revising a semantic file for your enterprise.
+You will receive an initial shell of a semantic file for Cortex Analyst and must update the semantic file using additional metadata files.
+The generated Cortex Analyst semantic file MUST adhere to the following documentation:
+<docs>
+{docs}
+</docs>
+Follow the rules below.
+<rules>
+1. Generated descriptions should be concise.
+2. Each tablename should correspond to a single logical table in the semantic file. Do not create multiple logical tables for a single tablename.
+3. Do not make assumptions about filters. Table samples are not exhaustive of values.
+</rules>
+<initial_semantic_shell>
+{initial_semantic_file}
+</initial_semantic_shell>
+<other_metadata_files>
+{metadata_files}
+</other_metadata_files>
+Revised Semantic File: 
+"""
+
+def run_cortex_complete(conn: SnowflakeConnection,
+                        model: str,
+                        prompt: str,
+                        prompt_args: Optional[dict] = None) -> str:
+    
+    if prompt_args:
+        prompt = prompt.format(**prompt_args)# .replace("'", "\\'")
+    complete_sql = f"SELECT snowflake.cortex.complete('{model}', '{prompt}')"
+    # response = conn.cursor().execute(complete_sql).fetchone()[0]
+
+    # return response
+    return prompt
+
+def format_metadata_files(metadata_files: dict) -> str:
+    metadata_str = ""
+    for fname, metadata in metadata_files.items():
+        metadata_str += f"Filename: {metadata['filename']}\n"
+        metadata_str += f"Platform: {metadata['platform']}\n"
+        metadata_str += f"Contents: {metadata['contents']}\n"
+    return metadata_str
+
+
+def get_cortex_analyst_docs(webpage: str = "https://docs.snowflake.com/LIMITEDACCESS/snowflake-cortex/semantic-model-spec") -> str:
+    # TODO: Slice the webpage to get the relevant documentation 
+    import requests
+    from bs4 import BeautifulSoup
+
+    r = requests.get(webpage)
+    soup = BeautifulSoup(r.text, "html.parser")
+    article = soup.find("article")
+    sections = article.find_all('section')
+    docs = ""
+    for section in sections:
+         section_id = section.get('id')
+         for copybutton in section.find_all(class_='copybutton'):
+            copybutton.decompose()
+         if section_id in ['key-concepts',
+                           'tips-for-creating-a-semantic-model',
+                           'specification',
+                           'example-yaml']:
+            section_text = section.get_text(strip=False)
+            docs += f"{section_text}\n"
+            docs = docs.replace("\n\n", "\n")
+    return docs
+
+def refine_with_other_metadata(conn: SnowflakeConnection,
+                               model: str = 'mistral-large',
+                               prompt: str = revise_semantic_prompt,
+                               prompt_args: Optional[dict] = None) -> str:
+    
+    return run_cortex_complete(conn, model=model, prompt = prompt, prompt_args=prompt_args)
+                                            
--- admin_apps/journeys/builder.py
+++ admin_apps/journeys/builder.py
@@ -5,6 +5,7 @@ from semantic_model_generator.generate_model import generate_model_str_from_snow
 from semantic_model_generator.snowflake_utils.snowflake_connector import (
     fetch_table_names,
 )
+from admin_apps.curate_semantic import format_metadata_files, get_cortex_analyst_docs, refine_with_other_metadata
 
 
 @st.cache_resource(show_spinner=False)
@@ -27,46 +28,85 @@ def table_selector_dialog() -> None:
     st.write(
         "Please fill out the following fields to start building your semantic model."
     )
-    with st.form("table_selector_form"):
-        model_name = st.text_input("Semantic Model Name")
-        sample_values = st.selectbox(
-            "Number of sample values", list(range(1, 11)), index=0
-        )
-        st.markdown("")
+    # with st.form("table_selector_form"):
+    model_name = st.text_input("Semantic Model Name")
+    sample_values = st.selectbox(
+        "Number of sample values", list(range(1, 11)), index=0
+    )
+    st.markdown("")
 
-        if "available_tables" not in st.session_state:
-            with st.spinner("Loading table definitions..."):
-                st.session_state["available_tables"] = get_available_tables()
+    if "available_tables" not in st.session_state:
+        with st.spinner("Loading table definitions..."):
+            st.session_state["available_tables"] = get_available_tables()
 
-        tables = st.multiselect(
-            label="Tables",
-            options=st.session_state["available_tables"],
-            placeholder="Select the tables you'd like to include in your semantic model.",
-        )
-        st.markdown("<div style='margin: 240px;'></div>", unsafe_allow_html=True)
-        submit = st.form_submit_button(
-            "Submit", use_container_width=True, type="primary"
-        )
-        if submit:
-            if not model_name:
-                st.error("Please provide a name for your semantic model.")
-            elif not tables:
-                st.error("Please select at least one table to proceed.")
-            else:
-                with st.spinner("Generating model..."):
-                    yaml_str = generate_model_str_from_snowflake(
-                        base_tables=tables,
-                        snowflake_account=st.session_state["account_name"],
-                        semantic_model_name=model_name,
-                        n_sample_values=sample_values,  # type: ignore
-                        conn=get_snowflake_connection(),
-                    )
+    tables = st.multiselect(
+        label="Tables",
+        options=st.session_state["available_tables"],
+        placeholder="Select the tables you'd like to include in your semantic model.",
+    )
 
-                    # Set the YAML session state so that the iteration app has access to the generated contents,
-                    # then proceed to the iteration screen.
-                    st.session_state["yaml"] = yaml_str
-                    st.session_state["page"] = GeneratorAppScreen.ITERATION
-                    st.rerun()
+    has_semantic = st.radio("Do you have existing semantic or reporting layers?", ("No", "Yes"))
+    if has_semantic == "Yes":
+        metadata_files = st.file_uploader('Upload metadata files', 
+                                    accept_multiple_files=True)
+        if metadata_files:
+            metadata = {}                      
+            for uploaded_file in metadata_files:
+                fname = uploaded_file.name
+                metadata[fname] = {}
+                c1, c2, c3 = st.columns(3)
+                with c1:
+                    st.write(fname)
+                with c2:
+                    metadata[fname]['filename'] = st.text_input(f"filename_{fname}",
+                                             placeholder = "Descriptive filename",
+                                             label_visibility="collapsed")
+                with c3:
+                    metadata[fname]['platform'] = st.text_input(f"platform_{fname}",
+                                             placeholder = "Enter source platform",
+                                             label_visibility="collapsed")
+                metadata[fname]['contents'] = uploaded_file.read().decode('utf-8')
+            st.session_state["metadata_files"] = metadata
+    else:
+        st.session_state["metadata_files"] = None
+
+    st.markdown("<div style='margin: 240px;'></div>", unsafe_allow_html=True)
+    submit = st.button(
+        "Submit", use_container_width=True, type="primary"
+    )
+    if submit:
+        if not model_name:
+            st.error("Please provide a name for your semantic model.")
+        elif not tables:
+            st.error("Please select at least one table to proceed.")
+        else:
+            with st.spinner("Generating model..."):
+                # yaml_str = generate_model_str_from_snowflake(
+                #     base_tables=tables,
+                #     snowflake_account=st.session_state["account_name"],
+                #     semantic_model_name=model_name,
+                #     n_sample_values=sample_values,  # type: ignore
+                #     conn=get_snowflake_connection(),
+                # )
+                yaml_str =  'abc'
+                # Set the YAML session state so that the iteration app has access to the generated contents,
+                # then proceed to the iteration screen.
+                # st.session_state["yaml"] = yaml_str
+                # st.session_state["page"] = GeneratorAppScreen.ITERATION
+                
+                if st.session_state["metadata_files"]:
+                    metadata_str = format_metadata_files(st.session_state["metadata_files"])
+                    prompt_args = {
+                        "docs": get_cortex_analyst_docs(),
+                        "initial_semantic_file": yaml_str,
+                        "metadata_files": metadata_str
+                    }
+                    response = refine_with_other_metadata(conn = get_snowflake_connection(),
+                                                          prompt_args = prompt_args)
+
+                    # st.write(response)
+                    st.text_area("Prompt", response, height=500)
+                # st.rerun()
 
 
 def show() -> None:
