2f1f6750137683dcaa45da856b6f4451ce0777e4
Chris Nivera <chris.nivera@snowflake.com>
2024-10-23 15:18:36 -0700

    Call Analyst to perform validation (#187)
    We currently maintain copies of the validation logic in both the
    internal Analyst codepaths as well as this OSS app. Often, the OSS app
    can become out of date. Instead of performing validation locally, we
    will simply call Analyst with the current YAML string, as it performs
    validation at inference time. Any error returned is shown to the user.
    
    The diff for this PR seems big but it's mostly deleting unnecessary code
    + tests.

---
+++ app_utils/chat.py
@@ -0,0 +1,82 @@
+import json
+import re
+from typing import Dict, Any
+
+import requests
+import streamlit as st
+from snowflake.connector import SnowflakeConnection
+
+API_ENDPOINT = "https://{HOST}/api/v2/cortex/analyst/message"
+
+
+@st.cache_data(ttl=60, show_spinner=False)
+def send_message(
+    _conn: SnowflakeConnection, semantic_model: str, messages: list[dict[str, str]]
+) -> Dict[str, Any]:
+    """
+    Calls the REST API with a list of messages and returns the response.
+    Args:
+        _conn: SnowflakeConnection, used to grab the token for auth.
+        messages: list of chat messages to pass to the Analyst API.
+        semantic_model: stringified YAML of the semantic model.
+
+    Returns: The raw ChatMessage response from Analyst.
+    """
+    request_body = {
+        "messages": messages,
+        "semantic_model": semantic_model,
+    }
+
+    if st.session_state["sis"]:
+        import _snowflake
+
+        resp = _snowflake.send_snow_api_request(  # type: ignore
+            "POST",
+            f"/api/v2/cortex/analyst/message",
+            {},
+            {},
+            request_body,
+            {},
+            30000,
+        )
+        if resp["status"] < 400:
+            json_resp: Dict[str, Any] = json.loads(resp["content"])
+            return json_resp
+        else:
+            err_body = json.loads(resp["content"])
+            if "message" in err_body:
+                # Certain errors have a message payload with a link to the github repo, which we should remove.
+                error_msg = re.sub(
+                    r"\s*Please use https://github\.com/Snowflake-Labs/semantic-model-generator.*",
+                    "",
+                    err_body["message"],
+                )
+                raise ValueError(error_msg)
+            raise ValueError(err_body)
+
+    else:
+        host = st.session_state.host_name
+        resp = requests.post(
+            API_ENDPOINT.format(
+                HOST=host,
+            ),
+            json=request_body,
+            headers={
+                "Authorization": f'Snowflake Token="{_conn.rest.token}"',  # type: ignore[union-attr]
+                "Content-Type": "application/json",
+            },
+        )
+        if resp.status_code < 400:
+            json_resp: Dict[str, Any] = resp.json()
+            return json_resp
+        else:
+            err_body = json.loads(resp.text)
+            if "message" in err_body:
+                # Certain errors have a message payload with a link to the github repo, which we should remove.
+                error_msg = re.sub(
+                    r"\s*Please use https://github\.com/Snowflake-Labs/semantic-model-generator.*",
+                    "",
+                    err_body["message"],
+                )
+                raise ValueError(error_msg)
+            raise ValueError(err_body)
--- semantic_model_generator/validate_model.py
+++ semantic_model_generator/validate_model.py
@@ -1,14 +1,8 @@
-from loguru import logger
-from snowflake.connector import SnowflakeConnection
+import json
+import re
 
-from semantic_model_generator.data_processing.cte_utils import (
-    context_to_column_format,
-    expand_all_logical_tables_as_ctes,
-    generate_select,
-    validate_all_cols,
-)
-from semantic_model_generator.data_processing.proto_utils import yaml_to_semantic_model
-from semantic_model_generator.validate.context_length import validate_context_length
+from snowflake.connector import SnowflakeConnection
+from app_utils.chat import send_message
 
 
 def load_yaml(yaml_path: str) -> str:
@@ -24,46 +18,20 @@ def load_yaml(yaml_path: str) -> str:
 
 def validate(yaml_str: str, conn: SnowflakeConnection) -> None:
     """
-    For now, validate just ensures that the yaml is correctly formatted and we can parse into our protos.
+    We perform pseudo-validation by issuing a request to Cortex Analyst with the YAML string as-is, and determining
+    whether the request is successful. We don't currently have an explicit validation endpoint available, but validation
+    is run at inference time, so this is a reasonable proxy.
+
+    This is done in order to remove the need to sync validation logic locally between these codepaths and Analyst.
 
     yaml_str: yaml content in string format.
     conn: SnowflakeConnection Snowflake connection to pass in
-
-    TODO: ensure that all expressions are valid by running a query containing all columns and expressions.
     """
 
-    model = yaml_to_semantic_model(yaml_str)
-    # Validate the context length doesn't exceed max we can support.
-    validate_context_length(model, throw_error=True)
-
-    model_in_column_format = context_to_column_format(model)
-
-    for table in model_in_column_format.tables:
-        logger.info(f"Checking logical table: {table.name}")
-        try:
-            validate_all_cols(table)
-            sqls = generate_select(table, 1)
-            # Run the query.
-            # TODO: some expr maybe expensive if contains aggregations or window functions. Move to EXPLAIN?
-            for sql in sqls:
-                _ = conn.cursor().execute(sql)
-        except Exception as e:
-            raise ValueError(f"Unable to validate your semantic model. Error = {e}")
-        logger.info(f"Validated logical table: {table.name}")
-
-    for vq in model.verified_queries:
-        logger.info(f"Checking verified queries for: {vq.question}")
-        try:
-            vqr_with_ctes = expand_all_logical_tables_as_ctes(
-                vq.sql, model_in_column_format
-            )
-            # Run the query
-            _ = conn.cursor().execute(vqr_with_ctes)
-        except Exception as e:
-            raise ValueError(f"Fail to validate your verified query. Error = {e}")
-        logger.info(f"Validated verified query: {vq.question}")
-
-    logger.info("Successfully validated!")
+    dummy_request = [
+        {"role": "user", "content": [{"type": "text", "text": "SMG app validation"}]}
+    ]
+    send_message(conn, yaml_str, dummy_request)
 
 
 def validate_from_local_path(yaml_path: str, conn: SnowflakeConnection) -> None:
